61a8b48098ca8d7b52a2cb7adc8059f5e60e8f78
Suzuki Jhordan <jhordan.kendy@outlook.com>
2022-10-24 02:23:04 -0300

    improving CSS

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
-// 804510e0033e4d65eefbf1ff697d676c
+const apiKey = "804510e0033e4d65eefbf1ff697d676c"
+const apiCountryURL = "https://countryflagsapi.com/png/"
 
-console.log('oi eu sou o goku')
+const cityInput = document.querySelector('#city-input')
+const searchBtn = document.querySelector('#search')
+
+const cityElement = document.querySelector('#city')
+const tempElement = document.querySelector('#temperature span')
+const descElement = document.querySelector('#description')
+const weatherIconElement = document.querySelector('#weather-icon')
+const countryElement = document.querySelector('#country')
+const humidityElement = document.querySelector('#humidity span')
+const windElement = document.querySelector('#wind span')
+
+const getWeatherData = async(city) => {
+    const apiWeatherURL = `https://api.openweathermap.org/data/3.0/onecall?q=${city}`
+}
+
+const showWeatherData = event =>{
+    event.preventDefault()
+
+    const city = cityInput.value 
+    console.log(city);
+}
+
+searchBtn.addEventListener('click', showWeatherData)

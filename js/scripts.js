6f175bfdb1e9c7934a27b80934fbf1cf0b01df9a
Suzuki Jhordan <jhordan.kendy@outlook.com>
2022-10-24 18:45:06 -0300

    js

--- js/scripts.js
+++ js/scripts.js
@@ -14,15 +14,28 @@ const windElement = document.querySelector('#wind span')
 
 const getWeatherData = async city => {
     const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
-    const response = await fetch(apiWeatherURL)
-    return(response.json())
+    
+    try{
+        const response = await fetch(apiWeatherURL)
+        const data = await response.json()
+        return data
+
+    }catch(err){
+        console.log(`erro:${err}`)
+    }
+
 }
 
-const showWeatherData = event =>{
+const getWeatherCity =   async event =>{
     event.preventDefault()
-
-    const city = cityInput.value 
-    getWeatherData(city)
+    const city = cityInput.value
+    const data = await getWeatherData(city) 
+    showWeatherData(data)
 }
 
-searchBtn.addEventListener('click', showWeatherData)
+const showWeatherData =  data => {
+    cityElement.innerText =  data.name
+    console.log(data)
+}
+
+searchBtn.addEventListener('click', getWeatherCity)

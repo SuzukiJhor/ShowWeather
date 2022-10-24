54ef1837abc8898090d08945e79e952ea5ae7d14
Suzuki Jhordan <jhordan.kendy@outlook.com>
2022-10-24 02:57:19 -0300

    js

--- js/scripts.js
+++ js/scripts.js
@@ -12,15 +12,17 @@ const countryElement = document.querySelector('#country')
 const humidityElement = document.querySelector('#humidity span')
 const windElement = document.querySelector('#wind span')
 
-const getWeatherData = async(city) => {
-    const apiWeatherURL = `https://api.openweathermap.org/data/3.0/onecall?q=${city}`
+const getWeatherData = async city => {
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+    const response = await fetch(apiWeatherURL)
+    return(response.json())
 }
 
 const showWeatherData = event =>{
     event.preventDefault()
 
     const city = cityInput.value 
-    console.log(city);
+    getWeatherData(city)
 }
 
 searchBtn.addEventListener('click', showWeatherData)

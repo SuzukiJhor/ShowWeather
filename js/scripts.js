739bcbd3c0dbec7bc49c2edb468bd742aa28cc72
Suzuki Jhordan <jhordan.kendy@outlook.com>
2022-10-25 16:25:46 -0300

    getImage

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,47 @@
-const apiKey = "804510e0033e4d65eefbf1ff697d676c"
+const apiWeatherKey = "804510e0033e4d65eefbf1ff697d676c"
 const apiCountryURL = "https://countryflagsapi.com/png/"
+const apiImageKey = '30854610-0a230a3f670569a8f29cc4441'
 
 const cityInput = document.querySelector('#city-input')
 const searchBtn = document.querySelector('#search')
-const humidityElement = document.querySelector('#humidity span')
-const windElement = document.querySelector('#wind span')
 
-const getWeatherData = async city => {
-    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+const showImage = data => {
+    console.log(data)
+}
+
+const getImageCity =  async text => {
+    const apiImageUrl =  `https://pixabay.com/api/?key=${apiImageKey}&q=${text}&image_type=photo`
     
-    try{
-        const response = await fetch(apiWeatherURL)
-        const data = await response.json()
-        return data
-
-    }catch(err){
-        console.log(`erro:${err}`)
-    }
-
+    const response = await fetch(apiImageUrl)
+    const data = await response.json()
+    return data
 }
 
 const getWeatherCity =   async event =>{
     event.preventDefault()
     const city = cityInput.value
     const data = await getWeatherData(city) 
+    const imageData = await getImageCity(city)
     showWeatherData(data)
+    showImage(imageData)
+}
+
+const getWeatherByEnter = async event =>{
+    if(event.code === 'Enter'){
+        const city = event.target.value
+        const data = await getWeatherData(city)
+        const imageData = await getImageCity(city)
+        showWeatherData(data)
+        showImage(imageData)
+    }
+}
+
+const getWeatherData = async city => {
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiWeatherKey}`
+    
+    const response = await fetch(apiWeatherURL)
+    const data = await response.json()
+    return data
 }
 
 const showWeatherData =  data => {
@@ -52,6 +69,9 @@ const showWeatherData =  data => {
 
     document.querySelector('#wind span')
     .innerText = `${data.wind.speed}km/h`
+
+    document.querySelector('#weather-data').classList.remove('hide')
 }
 
 searchBtn.addEventListener('click', getWeatherCity)
+cityInput.addEventListener('keyup', getWeatherByEnter)
